d1ff9d86fe79d93a0ad9091173ea9e8345c55fff
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2022-02-23 07:16:49 +0100

    abstract game class to separate file

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,56 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using Rule = Rules;
-using Word = BuzzWords;
+﻿using Game = FizzBuzz.Game;
 
 Main();
 
 static void Main()
 {
+    //del_1();
+    del_2();
+}
+
+static void del_1()
+{
     Game game = new Game();
+
+    Game.Iterator();
+}
+
+static void del_2()
+{
+    Game game = new Game();
+
     Game.Iterator();
     Game.ReverseIterator();
 }
 
-public class Game
-{
-    private static int iteratorSize = 100;
-    public static void Iterator()
-    {
-        for (int i = 1; i < iteratorSize; i++)
-        {
-            if (Rule.divisibleByThree(i) && Rule.divisibleByFive(i))
-            {
-                Console.WriteLine(Word.FizzBuzz());
-            }
-            else if (Rule.divisibleByThree(i))
-            {
-                Console.WriteLine(Word.Fizz());
-            }
-            else if (Rule.divisibleByFive(i))
-            {
-                Console.WriteLine(Word.Buzz());
-            }
-            else
-                Console.WriteLine(i);
-        }
-    }
-
-    public static void ReverseIterator()
-    {
-        for (int i = iteratorSize; i > 1; i--)
-        {
-            if (Rule.divisibleByNine(i))
-            {
-                Console.WriteLine(Word.Jazz());
-            }
-            else if (Rule.divisibleByFour(i))
-            {
-                Console.WriteLine(Word.Fuzz());
-            }
-            else
-                Console.WriteLine(i);
-        }
-    }
-}
 
---
+++ FizzBuzz/Game.cs
@@ -0,0 +1,47 @@
+﻿using Rule = Rules;
+using Word = BuzzWords;
+
+namespace FizzBuzz
+{
+    public class Game
+    {
+        private static readonly int iteratorSize = 100;
+        public static void Iterator()
+        {
+            for (int i = 1; i < iteratorSize; i++)
+            {
+                if (Rule.divisibleByThree(i) && Rule.divisibleByFive(i))
+                {
+                    Console.WriteLine(Word.FizzBuzz());
+                }
+                else if (Rule.divisibleByThree(i))
+                {
+                    Console.WriteLine(Word.Fizz());
+                }
+                else if (Rule.divisibleByFive(i))
+                {
+                    Console.WriteLine(Word.Buzz());
+                }
+                else
+                    Console.WriteLine(i);
+            }
+        }
+
+        public static void ReverseIterator()
+        {
+            for (int i = iteratorSize; i > 1; i--)
+            {
+                if (Rule.divisibleByNine(i))
+                {
+                    Console.WriteLine(Word.Jazz());
+                }
+                else if (Rule.divisibleByFour(i))
+                {
+                    Console.WriteLine(Word.Fuzz());
+                }
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
