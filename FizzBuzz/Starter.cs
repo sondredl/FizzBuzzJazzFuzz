70c33480e44117592ccec71426580ba8f423a087
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2022-02-23 19:22:33 +0100

    wrap main in class Starter

---
+++ FizzBuzz/Starter.cs
@@ -0,0 +1,30 @@
+﻿using Game = FizzBuzz.Game;
+
+Starter.Main();
+
+public class Starter
+{
+
+    public static void Main()
+    {
+        //del_1();
+        del_2();
+    }
+
+    static void del_1()
+    {
+        Game game = new Game();
+
+        Game.Iterator();
+    }
+
+    static void del_2()
+    {
+        Game game = new Game();
+
+        Game.Iterator();
+        Game.ReverseIterator();
+    }
+}
+
+
