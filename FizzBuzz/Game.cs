b91e3b81954c30c88982869c04b609d496674d11
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2022-02-25 14:15:21 +0100

    move class Game() to Game.cs and code formating

--- FizzBuzz/Game.cs
+++ FizzBuzz/Game.cs
@@ -1,7 +1,59 @@
 ﻿
 namespace FizzBuzz
 {
+    public class Game
+    {
+        public static void Del_1(Game game, int iteratorSize)
+        {
+            Game.Iterator(iteratorSize);
+        }
 
+        public static void Del_2(Game game, int iteratorSize)
+        {
+            Game.Iterator(iteratorSize);
+            ReverseIterator(iteratorSize);
+        }
+
+        static void Iterator(int iteratorSize)
+        {
+            for (int i = 1; i < iteratorSize; i++)
+            {
+                if (Rules.DivisibleByThree(i) && Rules.DivisibleByFive(i))
+                {
+                    Console.WriteLine($"{BuzzWords.fizz}{BuzzWords.buzz}");
+                }
+                else if (Rules.DivisibleByThree(i))
+                {
+                    Console.WriteLine(BuzzWords.fizz);
+                }
+                else if (Rules.DivisibleByFive(i))
+                {
+                    Console.WriteLine(BuzzWords.buzz);
+                }
+                else
+                    Console.WriteLine(i);
+            }
+        }
+
+        static void ReverseIterator(int iteratorSize)
+        {
+            for (int i = iteratorSize; i > 1; i--)
+            {
+                if (Rules.DivisibleByNine(i))
+                {
+                    Console.WriteLine(BuzzWords.jazz);
+                }
+                else if (Rules.DivisibleByFour(i))
+                {
+                    Console.WriteLine(BuzzWords.fuzz);
+                }
+                else
+                    Console.WriteLine(i);
+            }
+        }
+
+
+    }
     public class Rules
     {
         static public bool DivisibleByThree(int i)
--- FizzBuzz/Main.cs
+++ FizzBuzz/Main.cs
@@ -1,5 +1,6 @@
-﻿using Rule = FizzBuzz.Rules;
-using Word = FizzBuzz.BuzzWords;
+﻿using Game = FizzBuzz.Game;
+//using Rule = FizzBuzz.Rules;
+//using Word = FizzBuzz.BuzzWords;
 
 Main();
 
@@ -12,61 +13,6 @@ static void Main()
 
     Game game_2 = new Game();
     Game.Del_2(game_2, iteratorSize);
-}
-public class Game
-{
-    public static void Del_1(Game game, int iteratorSize)
-    {
-
-        Game.Iterator(iteratorSize);
-    }
-
-    public static void Del_2(Game game, int iteratorSize)
-    {
-        Game.Iterator(iteratorSize);
-        ReverseIterator(iteratorSize);
-    }
-
-
-    static void Iterator(int iteratorSize)
-    {
-        for (int i = 1; i < iteratorSize; i++)
-        {
-            if (Rule.DivisibleByThree(i) && Rule.DivisibleByFive(i))
-            {
-                Console.WriteLine($"{Word.fizz}{Word.buzz}");
-            }
-            else if (Rule.DivisibleByThree(i))
-            {
-                Console.WriteLine(Word.fizz);
-            }
-            else if (Rule.DivisibleByFive(i))
-            {
-                Console.WriteLine(Word.buzz);
-            }
-            else
-                Console.WriteLine(i);
-        }
-    }
-
-    static void ReverseIterator(int iteratorSize)
-    {
-        for (int i = iteratorSize; i > 1; i--)
-        {
-            if (Rule.DivisibleByNine(i))
-            {
-                Console.WriteLine(Word.jazz);
-            }
-            else if (Rule.DivisibleByFour(i))
-            {
-                Console.WriteLine(Word.fuzz);
-            }
-            else
-                Console.WriteLine(i);
-        }
-    }
-
-
 }
 
 
