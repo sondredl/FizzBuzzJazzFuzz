8b5e462d1059fa7a30470dd114e4c7536392b9aa
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2022-02-28 23:22:11 +0100

    debugging Iterator()

--- FizzBuzz/Main.cs
+++ FizzBuzz/Main.cs
@@ -9,34 +9,36 @@ namespace FizzBuzz
         {
             int iteratorSize = 100;
 
+            List<KeyValuePair<string, int>> Fizz = KeyValue.FizzWords;
             Game game_1 = new();
-            game_1.Del_1(iteratorSize);
+            game_1.Del_1(Fizz, iteratorSize);
 
             Game game_2 = new();
-            game_2.Del_2(iteratorSize);
+            game_2.Del_2(Fizz, iteratorSize);
         }
     }
 
 
     public class Game
     {
-        public static void Del_1(int iteratorSize)
+        public static void Del_1(List<KeyValuePair<string, int>> Fizz, int iteratorSize)
         {
-            Iterator(iteratorSize);
+            Iterator(Fizz, iteratorSize);
         }
 
-        public static void Del_2(int iteratorSize)
-        {
-            Iterator(iteratorSize);
-            ReverseIterator(iteratorSize);
-        }
-        public static void Iterator(List<KeyValuePair<string, int>> Fizz, int limit)
-        {
-            Iterator(iteratorSize);
-            ReverseIterator(iteratorSize);
-        }
+        //public static void Del_2(List<KeyValuePair<string, int>> Fizz, int iteratorSize)
+        //{
+        //    Iterator(Fizz, iteratorSize);
+        //    //ReverseIterator(Fizz, iteratorSize);
+        //}
 
-        static void Iterator(KeyValuePair<string, int> Fizz, int iteratorSize)
+        //public static void Iterator(List<KeyValuePair<string, int>> Fizz, int limit)
+        //{
+        //    Iterator(iteratorSize);
+        //    ReverseIterator(iteratorSize);
+        //}
+
+        public static void Iterator(List<KeyValuePair<string, int>> Fizz, int iteratorSize)
             for (int i = 1; i<iteratorSize; i++)
             {
                 string s = "";
@@ -51,21 +53,20 @@ namespace FizzBuzz
                 s = "";
             }
         }
-        static void ReverseIterator(int limit)
-            for (int i = 1; i > limit; i++)
-{
-    string s = "";
-    foreach (KeyValuePair<string, int> kvp in Fizz)
-    {
-        int k = kvp.Value;
-        if (Rule.DivisibleByThree(i) && Rule.DivisibleByThree(k)) s += kvp.Key;
-        if (Rule.DivisibleByFive(i) && Rule.DivisibleByFive(k)) s += kvp.Key;
-    }
-    if (s.Length == 0) Console.WriteLine(i);
-    else Console.WriteLine(s);
-    s = "";
-}
-        }
+//        static void ReverseIterator(List<KeyValuePair<string, int>> Fizz, int iteratorSize)
+//            for (int i = 1; i > limit; i++)
+//{
+//    string s = "";
+//    foreach (KeyValuePair<string, int> kvp in Fizz)
+//    {
+//        int k = kvp.Value;
+//        if (Rule.DivisibleByThree(i) && Rule.DivisibleByThree(k)) s += kvp.Key;
+//        if (Rule.DivisibleByFive(i) && Rule.DivisibleByFive(k)) s += kvp.Key;
+//    }
+//    if (s.Length == 0) Console.WriteLine(i);
+//    else Console.WriteLine(s);
+//    s = "";
+//}
     }
 }
 
