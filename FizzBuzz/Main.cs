a1984988ae71bc12b75bda188031dac3e5bc8514
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2022-02-28 23:45:12 +0100

    fix ReverseIteration() logic

--- FizzBuzz/Main.cs
+++ FizzBuzz/Main.cs
@@ -9,7 +9,7 @@ namespace FizzBuzz
         {
             int iterSize = 100;
             List<KeyValuePair<string, int>> Fizz = KeyValue.FizzWords;
-            List<KeyValuePair<string, int>> Jazz = KeyValue.FizzWords;
+            List<KeyValuePair<string, int>> Jazz = KeyValue.JazzWords;
             Game.Iterator(Fizz, iterSize);
             Game game_1 = new();
             //game_1.Del_1(Fizz, iterSize);
@@ -28,7 +28,7 @@ namespace FizzBuzz
         public static void Del_2(List<KeyValuePair<string, int>> Fizz, List<KeyValuePair<string, int>> Jazz, int iteratorSize)
         {
             Iterator(Fizz, iteratorSize);
-            ReverseIterator(Fizz, Jazz, iteratorSize);
+            ReverseIterator(Jazz, iteratorSize);
         }
 
         public static void Iterator(List<KeyValuePair<string, int>> Fizz, int limit)
@@ -47,16 +47,16 @@ namespace FizzBuzz
                 s = "";
             }
         }
-        public static void ReverseIterator(List<KeyValuePair<string, int>> Fizz, List<KeyValuePair<string, int>> Jazz, int limit)
+        public static void ReverseIterator(List<KeyValuePair<string, int>> Jazz, int limit)
         {
-            for (int i = limit; i > 1; limit--)
+            for (int i = limit; 0 < i; i--)
             {
                 string s = "";
-                foreach (KeyValuePair<string, int> kvp in Fizz)
+                foreach (KeyValuePair<string, int> kvp in Jazz)
                 {
                     int k = kvp.Value;
-                    if (Rule.DivisibleByThree(i) && Rule.DivisibleByThree(k)) s += kvp.Key;
-                    if (Rule.DivisibleByFive(i) && Rule.DivisibleByFive(k)) s += kvp.Key;
+                    if (Rule.DivisibleByNine(i) && Rule.DivisibleByNine(k)) s += kvp.Key;
+                    if (Rule.DivisibleByFour(i) && Rule.DivisibleByFour(k)) s += kvp.Key;
                 }
                 if (s.Length == 0) Console.WriteLine(i);
                 else Console.WriteLine(s);
@@ -65,76 +65,3 @@ namespace FizzBuzz
         }
     }
 }
-
-//namespace FizzBuzz
-//{
-//    internal static class Program
-//    {
-//        private static void Main()
-//        {
-//            int iteratorSize = 100;
-
-//            List<KeyValuePair<string, int>> Fizz = KeyValue.FizzWords;
-//            Game game_1 = new();
-//            game_1.Del_1(Fizz, iteratorSize);
-
-//            Game game_2 = new();
-//            game_2.Del_2(Fizz, iteratorSize);
-//        }
-//    }
-
-
-//public class Game
-//{
-//    public static void Del_1(List<KeyValuePair<string, int>> Fizz, int iteratorSize)
-//{
-//    Iterator(Fizz, iteratorSize);
-//}
-
-//public static void Del_2(List<KeyValuePair<string, int>> Fizz, int iteratorSize)
-//{
-//    Iterator(Fizz, iteratorSize);
-//    //ReverseIterator(Fizz, iteratorSize);
-//}
-
-//public static void Iterator(List<KeyValuePair<string, int>> Fizz, int limit)
-//{
-//    Iterator(iteratorSize);
-//    ReverseIterator(iteratorSize);
-//}
-
-//public void Iterator(List<KeyValuePair<string, int>> Fizz, int iteratorSize)
-//    for (int i = 1; i<iteratorSize; i++)
-//    {
-//        string s = "";
-//        foreach (KeyValuePair<string, int> kvp in Fizz)
-//        {
-//            int k = kvp.Value;
-//            if (Rule.DivisibleByThree(i) && Rule.DivisibleByThree(k)) s += kvp.Key;
-//            if (Rule.DivisibleByFive(i) && Rule.DivisibleByFive(k)) s += kvp.Key;
-//        }
-//        if (s.Length == 0) Console.WriteLine(i);
-//        else Console.WriteLine(s);
-//        s = "";
-//    }
-//}
-//        static void ReverseIterator(List<KeyValuePair<string, int>> Fizz, int iteratorSize)
-//            for (int i = 1; i > limit; i++)
-//{
-//    string s = "";
-//    foreach (KeyValuePair<string, int> kvp in Fizz)
-//    {
-//        int k = kvp.Value;
-//        if (Rule.DivisibleByThree(i) && Rule.DivisibleByThree(k)) s += kvp.Key;
-//        if (Rule.DivisibleByFive(i) && Rule.DivisibleByFive(k)) s += kvp.Key;
-//    }
-//    if (s.Length == 0) Console.WriteLine(i);
-//    else Console.WriteLine(s);
-//    s = "";
-//}
-//}
-//}
-
-
-
-
