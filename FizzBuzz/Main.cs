52234134f615230decdf92703fc3a08c64786d2f
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2022-02-28 10:43:41 +0100

    test av list og dictionary, virker ikke

--- FizzBuzz/Main.cs
+++ FizzBuzz/Main.cs
@@ -8,34 +8,57 @@ namespace FizzBuzz
     {
         static void Main()
         {
+            int iterSize = 100;
+            KeyValuePair KV = KeyValue.Fizz;
             Game del_1 = new Game();
-            Game.FizzBuzz();
+            Game.Iterator(iterSize);
         }
     }
 
 
-    public class Game
+    public static class Game
     {
-        public static void FizzBuzz()
-        {
-            var fizzBuzz = new List<KeyValuePair<string, int>>
-            {
-                new ("Fizz", 3),
-                new ("Buzz", 5)
-            };
-            foreach (var val in fizzBuzz)
-            {
-                Console.WriteLine(val);
-            }
-        }
-        public static void Iterator(int limit)
+        //public static object KeyValue { get; private set; }
+
+        public static void Run() { }
+        public static void Iterator(int limit, List Fizz)
         {
             for (int i = 1; i < limit; i++)
             {
-                var i = Rule.LoopRules(limit);
+                //if (FizzBuzz[i].ContainsKey(i))
+                if (KeyValue.value() == i)
+                {
+                    Console.WriteLine(KeyValue[0]);
+                }
+                //var i = Rule.LoopRules(limit);
                 //Rules(i);
             }
         }
+    }
+    public static class KeyValue
+    {
+        public static List<KeyValuePair<string, int>> Fizz = new()
+        {
+            new("Fizz", 3),
+            new("Buzz", 5)
+        };
+        //foreach (var val in Fizz)
+        //{
+        //    Console.WriteLine(val);
+        //}
+        //public static List<Dictionary<string>> Buzz = new()
+
+
+
+        public static Dictionary<string, int> Buzz = new Dictionary<string, int>()
+        {
+            new KeyValuePair<string, int>()
+            {
+                "fizz",
+            }
+            //new("Fizz", 3),
+            //new("Buzz", 5)
+        };
     }
 }
 
